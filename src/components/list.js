5056c7b5b3d9accb148b95e4fb99d3d782f380b2
devLife <mirantrix80@gmail.com>
2019-06-28 22:24:26 -0500

    [Feature]:Add Incomplete, Complete, History List

--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "../App.css";
 
 class List extends Component {
+
     render() {
         return (
-            <div>
-                <ul>{this.props.todoHistory.map(item => (
-                    <li key={item.id}>{item.todo}</li>))}
-                </ul>
+            <div className='row'>
+                <div className='list'>
+                    <h4>Incomplete</h4>
+                    <ul className='column'>{this.props.incomplete.map(item => (
+                        <li key={item.id} onClick={ () => this.props.DeleteToDo(item.id, item.todo)}>{item.todo}</li>))}
+                    </ul>
+                </div>
+                <div className='list'>
+                    <h4>Completed</h4>
+                        <ul className='column'>{this.props.completed.map(item => (
+                            <li className='done' key={item.id} onClick={ () => this.props.IncompleteToDo(item.id, item.todo)}>{item.todo}</li>))}
+                        </ul>
+                </div>
+                <div className='list'>
+                    <h4>History</h4>
+                        <ul className='column'>{this.props.history.map(item => (
+                            <li key={item.id}>{item.todo}</li>))}
+                        </ul>
+                </div>
             </div>   
         );
     }
@@ -16,10 +33,19 @@ class List extends Component {
 
 const mapStateToProps = state => {
   return {
-    todoHistory: state.todoInput.todoHistory
+    incomplete: state.todoInput.incomplete,
+    completed: state.todoInput.completed,
+    history: state.todoInput.history
   };
 };
 
+const mapDispachToProps = dispatch => {
+    return {
+        DeleteToDo: (id, todo) => dispatch({type: 'DELETE_TODO', id: id, todo: todo}),
+        IncompleteToDo: (id, todo) => dispatch({type: 'INCOMPLETE_TODO', id: id, todo: todo}),
+    };
+};
 
-export default connect(mapStateToProps, null)(List);
+
+export default connect(mapStateToProps, mapDispachToProps)(List);
 
--- src/redux/reducers/toDoInput.js
+++ src/redux/reducers/toDoInput.js
@@ -1,6 +1,7 @@
 const initialState = {
-    todoHistory:[],
-    completed: false
+    history: [],
+    incomplete:[],
+    completed: []
   };
   
   const todoInput = (state=initialState, action) => {
@@ -10,10 +11,27 @@ const initialState = {
         case "TODO_INPUT":
           return {
             ...state,
-            todoHistory: state.todoHistory.concat({ id: Math.random(), todo: action.input })
+            incomplete: state.incomplete.concat({ id: Math.random(), todo: action.input }),
+            history: state.history.concat({ id: Math.random(), todo: action.input })
           };
           break;
-        } 
+        case "DELETE_TODO":
+          return {
+            ...state,
+            completed: state.completed.concat({ id: action.id, todo: action.todo }),
+            incomplete: newState.incomplete.filter((item)=> item.id !== action.id )
+          }
+          break;
+        case "INCOMPLETE_TODO":
+          return {
+            ...state,
+            incomplete: state.incomplete.concat({ id: action.id, todo: action.todo }),
+            completed: newState.completed.filter((item)=> item.id !== action.id )
+          }
+          break;
+        default:
+          
+    } 
     
     return newState;
   };
